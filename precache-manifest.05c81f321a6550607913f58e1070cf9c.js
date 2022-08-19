8fe576cd87056ca3430cc84be6dd3f8a8cdccfa4
L1M05 <laxmansinghn666@gmail.com>
2022-08-19 16:22:21 +0530

    Updates

---
+++ precache-manifest.05c81f321a6550607913f58e1070cf9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d6bbeca93b5cd37791",
+    "url": "/TextUtils/static/css/main.364efbc7.chunk.css"
+  },
+  {
+    "revision": "58d6bbeca93b5cd37791",
+    "url": "/TextUtils/static/js/main.58d6bbec.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/TextUtils/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "d0ea2cb01f342501784e",
+    "url": "/TextUtils/static/js/2.d0ea2cb0.chunk.js"
+  },
+  {
+    "revision": "5d16b3ee060be9659cfd",
+    "url": "/TextUtils/static/js/runtime~main.5d16b3ee.js"
+  },
+  {
+    "revision": "1445dc959eef0712138aeca7114bd0b5",
+    "url": "/TextUtils/static/media/txtutil.1445dc95.png"
+  },
+  {
+    "revision": "920875760d667742128494c881c12557",
+    "url": "/TextUtils/static/media/clipboard.92087576.svg"
+  },
+  {
+    "revision": "b47305903499e5300cb5c87f5ea239b7",
+    "url": "/TextUtils/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TextUtils/precache-manifest.fd5e1e9587a08e2f92a2e02ec0969477.js"
+  "/TextUtils/precache-manifest.05c81f321a6550607913f58e1070cf9c.js"
 );
 
 workbox.clientsClaim();
